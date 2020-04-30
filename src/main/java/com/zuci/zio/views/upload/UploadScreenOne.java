956f3f8358bd55237b6ca95210284f9a7e24f4eb
sanjeeya <sanjeeya.v@zucisystems.com>
2020-04-30 12:01:04 +0530

    modified upload view

--- src/main/java/com/zuci/zio/views/upload/UploadScreenOne.java
+++ src/main/java/com/zuci/zio/views/upload/UploadScreenOne.java
@@ -31,6 +31,7 @@ import com.vaadin.flow.component.orderedlayout.HorizontalLayout;
 import com.vaadin.flow.component.orderedlayout.VerticalLayout;
 import com.vaadin.flow.component.tabs.Tab;
 import com.vaadin.flow.component.tabs.Tabs;
+import com.vaadin.flow.component.tabs.TabsVariant;
 import com.vaadin.flow.component.textfield.TextArea;
 import com.vaadin.flow.component.textfield.TextField;
 import com.vaadin.flow.component.upload.Upload;
@@ -48,342 +49,343 @@ import com.zuci.zio.views.main.MainView;
 @Route(value = "upload-screen-one", layout = MainView.class)
 @PageTitle("Upload")
 @CssImport(value = "./styles/my-grid-styles.css", themeFor = "vaadin-grid")
+@CssImport(value = "./styles/upload.css")
+@CssImport(value = "./styles/upload.css", themeFor = "vaadin-button")
+@CssImport(value = "./styles/tabs.css", themeFor = "vaadin-tabs")
 public class UploadScreenOne extends Div implements AfterNavigationObserver {
-	
+
 	@Autowired
 	private static PipelineConfigDao pipelineConfigDao;
 
 	private VerticalLayout verticalLayout;
-	
+
 	static ConfigRead configRead = new ConfigRead();
 	private static String downloadPath = configRead.readProperty("downloadPath");
-	
+
 	private String fileName = "";
-	
+
 	private String pipelineName = "";
-	
+
 	private String extractFolderPath = "";
-	
+
 	private Button next = new Button("Next");
-	
+
 	public UploadScreenOne(PipelineConfigDao pipelineConfigDao) throws IOException {
-		
+
 		this.pipelineConfigDao = pipelineConfigDao;
-		
+
 		next.addThemeVariants(ButtonVariant.LUMO_PRIMARY);
 		next.getStyle().set("margin-left", "90%");
 		next.getStyle().set("background", "#58d2cc");
 		next.getStyle().set("border-radius", "6px");
 		next.getStyle().set("color", "#4b483f");
-		
+
 		verticalLayout = new VerticalLayout();
 		verticalLayout.setHeightFull();
 		verticalLayout.setWidthFull();
-		
+
 		setUploadComponent();
-		
+
 		add(verticalLayout);
 	}
-	
+
 	public void setUploadComponent() {
-		
+
 		MemoryBuffer memoryBuffer = new MemoryBuffer();
-		
+
 		Upload upload = new Upload(memoryBuffer);
-		//upload.setMaxFileSize(1024*8388608);
-		
+		upload.addClassName("upload-layout");
+		// upload.setMaxFileSize(1024*8388608);
+
 		upload.addFinishedListener(e -> {
-			
+
 			fileName = memoryBuffer.getFileName();
-		    InputStream inputStream = memoryBuffer.getInputStream();
-		    
-		    downloadPath = downloadPath.replaceAll("user.home", System.getProperty("user.home"));
-		    System.out.println(downloadPath);
-		    
-		    File uploadFile = new File(downloadPath);
+			InputStream inputStream = memoryBuffer.getInputStream();
+
+			downloadPath = downloadPath.replaceAll("user.home", System.getProperty("user.home"));
+			System.out.println(downloadPath);
+
+			File uploadFile = new File(downloadPath);
 
 			if (!uploadFile.exists()) {
 				uploadFile.mkdirs();
 				System.out.println("Make Directory");
 			}
-			
+
 			try {
-				FileUtils.copyInputStreamToFile(inputStream, new File(downloadPath+fileName));
-				
-				new ZipFileHandling().unzip(downloadPath+fileName, downloadPath+fileName.substring(0, fileName.indexOf(".")));
-				
-				extractFolderPath = downloadPath+fileName.substring(0, fileName.indexOf("."));
+				FileUtils.copyInputStreamToFile(inputStream, new File(downloadPath + fileName));
+
+				new ZipFileHandling().unzip(downloadPath + fileName,
+						downloadPath + fileName.substring(0, fileName.indexOf(".")));
+
+				extractFolderPath = downloadPath + fileName.substring(0, fileName.indexOf("."));
 				System.out.println(extractFolderPath);
-				
-				File directory=new File(extractFolderPath);
-			    int fileCount = directory.list().length;
-			    
-			    System.out.println(fileCount);
-			    
-			    setTab();
-			    
+
+				File directory = new File(extractFolderPath);
+				int fileCount = directory.list().length;
+
+				System.out.println(fileCount);
+
+				setTab();
+
 			} catch (IOException e1) {
 				e1.printStackTrace();
 			}
-			
+
 		});
-		
+
 		verticalLayout.add(upload);
 	}
-	
+
 	void setTab() throws IOException {
-		
-		String data = ""; 
-		
+
+		String data = "";
+
 		Map<Tab, Component> tabsToPages = new HashMap<>();
 		Tabs tabs = new Tabs();
 		Div pages = new Div();
-		
-		//Div rootDiv = new Div();
-		
+
+		tabs.getStyle().set("width", "-webkit-fill-available");
+		tabs.getStyle().set("margin-bottom", "-18px");
+		tabs.addThemeVariants(TabsVariant.LUMO_EQUAL_WIDTH_TABS);
+		pages.getStyle().set("width", "-webkit-fill-available");
+		pages.getStyle().set("border-radius", "0px");
+
+		// Div rootDiv = new Div();
+
 		TextArea rootTextArea = new TextArea();
-		
+		rootTextArea.getStyle().set("border-radius", "0px");
+
 		File rootFileDirectory = new File(extractFolderPath);
-		
+
 		File[] rootFileList = rootFileDirectory.listFiles();
-		for(File f: rootFileList){
+		for (File f : rootFileList) {
 			System.out.println(f);
-			
-			if(!f.isDirectory()) {
-				data = new String(Files.readAllBytes(Paths.get(f.getAbsolutePath()))); 
+
+			if (!f.isDirectory()) {
+				data = new String(Files.readAllBytes(Paths.get(f.getAbsolutePath())));
 				pipelineName = f.getName().substring(0, f.getName().lastIndexOf("."));
-			    System.out.println(data);
-			    
-			    Tab rootTab = new Tab("Root");
-			    
-			    /*rootDiv.setText(data);
-			    rootDiv.setVisible(true);
-			    
-			    pages.add(rootDiv);
-			    
-			    tabsToPages.put(rootTab, rootDiv);*/
-			    
-			    rootTextArea.setValue("Root \n"+data);
-			    rootTextArea.setHeight("200px");
-			    rootTextArea.setWidth("1000px");
-			    
-			    rootTextArea.setVisible(true);
-			    
-			    pages.add(rootTextArea);
-			    
-			    tabsToPages.put(rootTab, rootTextArea);
-			    
-			    tabs.add(rootTab);
-			    break;
+				System.out.println(data);
+
+				Tab rootTab = new Tab("Root");
+
+				/*
+				 * rootDiv.setText(data); rootDiv.setVisible(true);
+				 * 
+				 * pages.add(rootDiv);
+				 * 
+				 * tabsToPages.put(rootTab, rootDiv);
+				 */
+
+				rootTextArea.setValue("Root \n" + data);
+				rootTextArea.setHeight("500px");
+				rootTextArea.setWidth("100%");
+
+				rootTextArea.setVisible(true);
+
+				pages.add(rootTextArea);
+
+				tabsToPages.put(rootTab, rootTextArea);
+
+				tabs.add(rootTab);
+				break;
 			}
-			
+
 		}
-		
+
 		int i = 1;
-		
-		for(File f: rootFileList){
+
+		for (File f : rootFileList) {
 			System.out.println(f);
-			
-			if(f.isDirectory()) {
+
+			if (f.isDirectory()) {
 				System.out.println(f);
 				File[] subProcess = f.listFiles();
-				
-				for(File subFile: subProcess){
-					if(!subFile.isDirectory()) {
-						data = new String(Files.readAllBytes(Paths.get(subFile.getAbsolutePath()))); 
-					    System.out.println(data);
-					    
-					    Tab calleeTab = new Tab("Callee "+i);
-					    
-					    /*Div calleeDiv = new Div();
-					    calleeDiv.setText(data);
-					    calleeDiv.setVisible(false);
-					    
-					    pages.add(calleeDiv);
-					    
-					    tabsToPages.put(calleeTab, calleeDiv);*/
-					    
-					    TextArea calleeTextArea = new TextArea();
-					    calleeTextArea.setValue("Callee "+i+"\n"+data);
-					    calleeTextArea.setHeight("200px");
-					    calleeTextArea.setWidth("1000px");
-					    
-					    calleeTextArea.setVisible(false);
-					    i++;
-					    
-					    pages.add(calleeTextArea);
-					    
-					    tabsToPages.put(calleeTab, calleeTextArea);
-					    
-					    tabs.add(calleeTab);
+
+				for (File subFile : subProcess) {
+					if (!subFile.isDirectory()) {
+						data = new String(Files.readAllBytes(Paths.get(subFile.getAbsolutePath())));
+						System.out.println(data);
+
+						Tab calleeTab = new Tab("Callee " + i);
+
+						/*
+						 * Div calleeDiv = new Div(); calleeDiv.setText(data);
+						 * calleeDiv.setVisible(false);
+						 * 
+						 * pages.add(calleeDiv);
+						 * 
+						 * tabsToPages.put(calleeTab, calleeDiv);
+						 */
+
+						TextArea calleeTextArea = new TextArea();
+						calleeTextArea.setValue("Callee " + i + "\n" + data);
+						calleeTextArea.setHeight("500px");
+						calleeTextArea.setWidth("100%");
+
+						calleeTextArea.setVisible(false);
+						i++;
+
+						pages.add(calleeTextArea);
+
+						tabsToPages.put(calleeTab, calleeTextArea);
+
+						tabs.add(calleeTab);
 					}
 				}
-					
+
 			}
 		}
-		
-		Set<Component> pagesShown  = Stream.of(rootTextArea)
-	            .collect(Collectors.toSet());
-		
+
+		Set<Component> pagesShown = Stream.of(rootTextArea).collect(Collectors.toSet());
+
 		tabs.addSelectedChangeListener(event -> {
-		    pagesShown.forEach(page -> page.setVisible(false));
-		    pagesShown.clear();
-		    Component selectedPage = tabsToPages.get(tabs.getSelectedTab());
-		    selectedPage.setVisible(true);
-		    pagesShown.add(selectedPage);
+			pagesShown.forEach(page -> page.setVisible(false));
+			pagesShown.clear();
+			Component selectedPage = tabsToPages.get(tabs.getSelectedTab());
+			selectedPage.setVisible(true);
+			pagesShown.add(selectedPage);
 		});
-		
+
 		verticalLayout.add(tabs);
 		verticalLayout.add(pages);
-		
+
 		setConfigurationComponent();
 	}
-	
+
 	public void setConfigurationComponent() {
-		
+
 		HorizontalLayout horizontalLayout = new HorizontalLayout();
-		
+
 		Label addLabel = new Label("Add Variable");
-		
+
 		Icon addIcon = new Icon(VaadinIcon.PLUS);
 		addIcon.getStyle().set("cursor", "pointer");
-		addIcon.addClickListener(
-		        event -> 
-		        {
-		        	verticalLayout.remove(next);
-		        	
-		        	HorizontalLayout addField = new HorizontalLayout();
-		    		
-		    		TextField localVariableField = new TextField();
-		    		localVariableField.setPlaceholder("Variable");
-		    		
-		    		
-		    		TextField localValueField = new TextField();
-		    		localValueField.setPlaceholder("Value");
-		    		
-		    		Icon saveIcon = new Icon(VaadinIcon.FILE_O);
-		    		saveIcon.getStyle().set("cursor", "pointer");
-		    		saveIcon.addClickListener(
-		    		        saveEvent ->
-		    		        {
-		    		        	saveVariable(localVariableField.getValue(),localValueField.getValue());
-		    		        	Notification.show(localVariableField.getValue());
-		    		        });
-		    		
-		    		Icon removeIcon = new Icon(VaadinIcon.CLOSE);
-		    		removeIcon.getStyle().set("cursor", "pointer");
-		    		removeIcon.addClickListener(
-		    		        removeEvent -> 
-		    		        {
-		    		        	addField.remove(localVariableField);
-		    		        	addField.remove(localValueField);
-		    		        	addField.remove(saveIcon);
-		    		        	addField.remove(removeIcon);
-		    		        	Notification.show("Removed");
-		    		        });
-		    		
-		    		addField.add(localVariableField);
-		    		addField.add(localValueField);
-		    		addField.add(saveIcon);
-		    		addField.add(removeIcon);
-		    		
-		    		verticalLayout.add(addField);
-		    		verticalLayout.add(next);
-		    		
-		        	Notification.show("added");
-		        });
-		
+		addIcon.addClickListener(event -> {
+			verticalLayout.remove(next);
+
+			HorizontalLayout addField = new HorizontalLayout();
+
+			TextField localVariableField = new TextField();
+			localVariableField.setPlaceholder("Variable");
+
+			TextField localValueField = new TextField();
+			localValueField.setPlaceholder("Value");
+
+			Icon saveIcon = new Icon(VaadinIcon.FILE_O);
+			saveIcon.getStyle().set("cursor", "pointer");
+			saveIcon.addClickListener(saveEvent -> {
+				saveVariable(localVariableField.getValue(), localValueField.getValue());
+				Notification.show(localVariableField.getValue());
+			});
+
+			Icon removeIcon = new Icon(VaadinIcon.CLOSE);
+			removeIcon.getStyle().set("cursor", "pointer");
+			removeIcon.addClickListener(removeEvent -> {
+				addField.remove(localVariableField);
+				addField.remove(localValueField);
+				addField.remove(saveIcon);
+				addField.remove(removeIcon);
+				Notification.show("Removed");
+			});
+
+			addField.add(localVariableField);
+			addField.add(localValueField);
+			addField.add(saveIcon);
+			addField.add(removeIcon);
+
+			verticalLayout.add(addField);
+			verticalLayout.add(next);
+
+			Notification.show("added");
+		});
+
 		horizontalLayout.add(addLabel);
 		horizontalLayout.add(addIcon);
-		
+
 		List<EditPipelineConfig> variableList = pipelineConfigDao.findByPipeline(pipelineName);
-		
-		int[] i = {1};
-		
+
+		int[] i = { 1 };
+
 		variableList.forEach((temp) -> {
 			HorizontalLayout addField = new HorizontalLayout();
-    		
-    		TextField localVariableField = new TextField();
-    		localVariableField.setPlaceholder("Variable");
-    		localVariableField.setValue(temp.getVariable());
-    		
-    		/*if(i[0] == 1) {
-    			localVariableField.setLabel("Variable");
-    		}*/
-    		
-    		
-    		TextField localValueField = new TextField();
-    		localValueField.setPlaceholder("Value");
-    		localValueField.setValue(temp.getValue());
-    		
-    		/*if(i[0] == 1) {
-    			localValueField.setLabel("Value");
-    		}*/
-    		
-    		//i[0]= i[0] + 1;
-    		
-    		Icon saveIcon = new Icon(VaadinIcon.FILE_O);
-    		saveIcon.getStyle().set("cursor", "pointer");
-    		saveIcon.addClickListener(
-    		        saveEvent ->
-    		        {
-    		        	saveVariable(localVariableField.getValue(),localValueField.getValue());
-    		        	Notification.show(localVariableField.getValue());
-    		        });
-    		
-    		Icon removeIcon = new Icon(VaadinIcon.CLOSE);
-    		removeIcon.getStyle().set("cursor", "pointer");
-    		removeIcon.addClickListener(
-    		        removeEvent -> 
-    		        {
-    		        	addField.remove(localVariableField);
-    		        	addField.remove(localValueField);
-    		        	addField.remove(saveIcon);
-    		        	addField.remove(removeIcon);
-    		        	Notification.show("Removed");
-    		        });
-    		
-    		addField.add(localVariableField);
-    		addField.add(localValueField);
-    		addField.add(saveIcon);
-    		addField.add(removeIcon);
-    		
-    		verticalLayout.add(addField);
-        });
-		
+
+			TextField localVariableField = new TextField();
+			localVariableField.setPlaceholder("Variable");
+			localVariableField.setValue(temp.getVariable());
+
+			/*
+			 * if(i[0] == 1) { localVariableField.setLabel("Variable"); }
+			 */
+
+			TextField localValueField = new TextField();
+			localValueField.setPlaceholder("Value");
+			localValueField.setValue(temp.getValue());
+
+			/*
+			 * if(i[0] == 1) { localValueField.setLabel("Value"); }
+			 */
+
+			// i[0]= i[0] + 1;
+
+			Icon saveIcon = new Icon(VaadinIcon.FILE_O);
+			saveIcon.getStyle().set("cursor", "pointer");
+			saveIcon.addClickListener(saveEvent -> {
+				saveVariable(localVariableField.getValue(), localValueField.getValue());
+				Notification.show(localVariableField.getValue());
+			});
+
+			Icon removeIcon = new Icon(VaadinIcon.CLOSE);
+			removeIcon.getStyle().set("cursor", "pointer");
+			removeIcon.addClickListener(removeEvent -> {
+				addField.remove(localVariableField);
+				addField.remove(localValueField);
+				addField.remove(saveIcon);
+				addField.remove(removeIcon);
+				Notification.show("Removed");
+			});
+
+			addField.add(localVariableField);
+			addField.add(localValueField);
+			addField.add(saveIcon);
+			addField.add(removeIcon);
+
+			verticalLayout.add(addField);
+		});
+
 		verticalLayout.add(horizontalLayout);
-		
-		/*HorizontalLayout horizontalLayout = new HorizontalLayout();
-		
-		TextField variableField = new TextField();
-		variableField.setPlaceholder("Variable");
-		
-		TextField valueField = new TextField();
-		valueField.setPlaceholder("Value");
-		
-		horizontalLayout.add(variableField);
-		horizontalLayout.add(valueField);*/
-		
+
+		/*
+		 * HorizontalLayout horizontalLayout = new HorizontalLayout();
+		 * 
+		 * TextField variableField = new TextField();
+		 * variableField.setPlaceholder("Variable");
+		 * 
+		 * TextField valueField = new TextField(); valueField.setPlaceholder("Value");
+		 * 
+		 * horizontalLayout.add(variableField); horizontalLayout.add(valueField);
+		 */
+
 		verticalLayout.add(horizontalLayout);
 		verticalLayout.add(next);
-		
+
 	}
-	
+
 	public void saveVariable(String variable, String value) {
-		
+
 		List<String> variableList = this.pipelineConfigDao.getVariableByPipeline(pipelineName);
 		System.out.println(variableList);
-		
-		if(variableList.contains(variable)) {
-			saveConfirmDialog(variable,value);
-		}else {
-			
+
+		if (variableList.contains(variable)) {
+			saveConfirmDialog(variable, value);
+		} else {
+
 		}
 	}
-	
-	//Confirmation Dialog
+
+	// Confirmation Dialog
 	private void saveConfirmDialog(String variable, String value) {
-		
+
 		Dialog dialog = new Dialog();
 
 		dialog.setCloseOnEsc(false);
@@ -393,16 +395,16 @@ public class UploadScreenOne extends Div implements AfterNavigationObserver {
 		messageLabel.setText("Variable already exists, Do you want to save?");
 
 		NativeButton confirmButton = new NativeButton("Confirm", event -> {
-			
-			if(this.pipelineConfigDao.updateVariableByPipeline(pipelineName, variable, value)) {
+
+			if (this.pipelineConfigDao.updateVariableByPipeline(pipelineName, variable, value)) {
 				Notification.show("Updated Successfully!");
-			}else {
+			} else {
 				Notification.show("Updation Fail!");
 			}
-			
+
 			dialog.close();
 		});
-		
+
 		confirmButton.getStyle().set("color", "#4b483f");
 		confirmButton.getStyle().set("background-color", "#58d2cc");
 		confirmButton.getStyle().set("padding", "0.5rem");
@@ -415,7 +417,7 @@ public class UploadScreenOne extends Div implements AfterNavigationObserver {
 		NativeButton cancelButton = new NativeButton("Cancel", event -> {
 			dialog.close();
 		});
-		
+
 		cancelButton.getStyle().set("color", "#4b483f");
 		cancelButton.getStyle().set("background-color", "#58d2cc");
 		cancelButton.getStyle().set("padding", "0.5rem");
@@ -425,15 +427,15 @@ public class UploadScreenOne extends Div implements AfterNavigationObserver {
 		cancelButton.getStyle().set("border", "none");
 		cancelButton.getStyle().set("font-weight", "600");
 
-		dialog.add(messageLabel,confirmButton, cancelButton);
+		dialog.add(messageLabel, confirmButton, cancelButton);
 
 		dialog.open();
 	}
-	
+
 	@Override
 	public void afterNavigation(AfterNavigationEvent event) {
 		// TODO Auto-generated method stub
-		
+
 	}
 
 }
