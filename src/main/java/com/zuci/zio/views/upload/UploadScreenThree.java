4577239e574811afe9f9495eb6d43564ab8fc5f7
sanjeeya <sanjeeya.v@zucisystems.com>
2020-05-06 18:24:42 +0530

    updating view in 3rd upload screen

--- src/main/java/com/zuci/zio/views/upload/UploadScreenThree.java
+++ src/main/java/com/zuci/zio/views/upload/UploadScreenThree.java
@@ -27,7 +27,7 @@ import com.zuci.zio.model.ChannelConfig;
 import com.zuci.zio.model.ChannelMaster;
 
 public class UploadScreenThree extends AppLayout {
-	
+
 	@Autowired
 	private static PipelineConfigDao pipelineConfigDao;
 
@@ -36,109 +36,110 @@ public class UploadScreenThree extends AppLayout {
 
 	@Autowired
 	private static CommonConfigDao commonConfigDao;
-	
+
 	private VerticalLayout verticalLayout;
-	
+
 	private String pipelineName = "";
 	private String fileName = "";
 	private String filePath = "";
-	
+
 	private TextField channelName = new TextField();
 	ComboBox<String> alias = new ComboBox<>();
 	private TextField variableCount = new TextField();
 	private TextField uploadFileName = new TextField();
-	
+
 	private Button run = new Button("Run");
 	private Button deploy = new Button("Deploy");
-	
+
 	private Grid<ChannelConfig> variableValueGrid;
-	
+
 	private Button viewLog = new Button("View Log");
 	private Button cancel = new Button("Cancel");
 	private Button back = new Button("Back");
 	private Button finish = new Button("Finish");
-	
+
 	public UploadScreenThree(PipelineConfigDao pipelineConfigDao, ChannelConfigDao channelConfigDao,
 			CommonConfigDao commonConfigDao, String pipelineName, String filePath) {
-		
+
 		this.pipelineConfigDao = pipelineConfigDao;
 		this.channelConfigDao = channelConfigDao;
 		this.commonConfigDao = commonConfigDao;
-		
+
 		this.pipelineName = pipelineName;
 		this.filePath = filePath;
-		this.fileName = filePath.substring(filePath.lastIndexOf("/")+1, filePath.length());
-		
+		this.fileName = filePath.substring(filePath.lastIndexOf("/") + 1, filePath.length());
+
 		channelName.setLabel("Name");
 		channelName.setEnabled(false);
-		
+
 		alias.setLabel("Alias");
-		
+
 		variableCount.setLabel("Variables");
 		variableCount.setEnabled(false);
-		
+
 		uploadFileName.setLabel("Upload File Name");
 		uploadFileName.setEnabled(false);
 		uploadFileName.setValue(this.fileName);
-		
+
 		verticalLayout = new VerticalLayout();
 		verticalLayout.setHeightFull();
 		verticalLayout.setWidthFull();
-		
+
 		setChannelConfiguration();
 		setRunDeployButton();
 		setTabAndGrid();
 		setFinalButtons();
-		
+
 		setContent(verticalLayout);
 	}
-	
+
 	public void setChannelConfiguration() {
-		
+
 		HorizontalLayout horizontalLayout;
-		
+
 		List<ChannelMaster> channelMasterList = this.channelConfigDao.findMasterByPipeline(pipelineName);
-		
+
 		List<String> aliasList = new ArrayList<String>();
-		
+
 		channelMasterList.forEach(e -> {
 			aliasList.add(e.getAlias());
 		});
-		
+
 		alias.setItems(aliasList);
-		
+
 		alias.addValueChangeListener(e -> {
 			System.out.println(e.getValue());
 			setChannelConfiguration(e.getValue());
 		});
-		
+
 		horizontalLayout = new HorizontalLayout();
 		horizontalLayout.add(channelName);
 		horizontalLayout.add(alias);
-		
+
 		verticalLayout.add(horizontalLayout);
-		
+
 		horizontalLayout = new HorizontalLayout();
 		horizontalLayout.add(variableCount);
 		horizontalLayout.add(uploadFileName);
-		
+
 		verticalLayout.add(horizontalLayout);
 	}
-	
+
 	public void setChannelConfiguration(String alias) {
-		
+
 		List<UploadPageThreeMasterDTO> channelVariableList = this.channelConfigDao.groupChannelByAlias(alias);
-		
-		if(!channelVariableList.isEmpty()) {
+
+		if (!channelVariableList.isEmpty()) {
 			channelName.setValue(channelVariableList.get(0).getInstance());
 			variableCount.setValue(String.valueOf(channelVariableList.get(0).getVariableCount()));
 		}
-		
-		variableValueGrid.setItems(channelConfigDao.findChannelByPipelineAndChannel(pipelineName, channelName.getValue()));
+
+		variableValueGrid
+				.setItems(channelConfigDao.findChannelByPipelineAndChannel(pipelineName, channelName.getValue()));
 	}
-	
+
 	public void setTabAndGrid() {
-		
+
 		Tab textLog = new Tab("Text Log");
 		Tab topLevel = new Tab("Top Level");
 		Tab macroLevel = new Tab("Macro Level");
@@ -165,60 +166,67 @@ public class UploadScreenThree extends AppLayout {
 		Grid.Column<ChannelConfig> valueColumn = variableValueGrid.addColumn(ChannelConfig::getValue)
 				.setHeader(new Html(
 						"<div style='font-weight:bold;font-size:16px;text-orientation: mixed;background:#fff;color:#4b483f'>Value</div>"));
-		
+
 		HorizontalLayout horizontalLayout = new HorizontalLayout();
 		horizontalLayout.setWidthFull();
 		horizontalLayout.add(tabs);
 		horizontalLayout.add(variableValueGrid);
-		
+
 		verticalLayout.add(horizontalLayout);
-		
+
 	}
-	
+
 	public void setRunDeployButton() {
-		
-		run.addThemeVariants(ButtonVariant.LUMO_PRIMARY);
-		run.getStyle().set("background", "#58d2cc");
-		run.getStyle().set("border-radius", "6px");
-		run.getStyle().set("color", "#4b483f");
-		
-		deploy.addThemeVariants(ButtonVariant.LUMO_PRIMARY);
-		deploy.getStyle().set("background", "#58d2cc");
-		deploy.getStyle().set("border-radius", "6px");
-		deploy.getStyle().set("color", "#4b483f");
-		
+		run.addClassName("add-button");
+		deploy.addClassName("delete-button");
+
+//		run.addThemeVariants(ButtonVariant.LUMO_PRIMARY);
+//		run.getStyle().set("background", "#58d2cc");
+//		run.getStyle().set("border-radius", "6px");
+//		run.getStyle().set("color", "#4b483f");
+
+//		deploy.addThemeVariants(ButtonVariant.LUMO_PRIMARY);
+//		deploy.getStyle().set("background", "#58d2cc");
+//		deploy.getStyle().set("border-radius", "6px");
+//		deploy.getStyle().set("color", "#4b483f");
+
 		HorizontalLayout horizontalLayout = new HorizontalLayout();
 		horizontalLayout.setWidthFull();
 		horizontalLayout.add(run);
 		horizontalLayout.add(deploy);
-		
+
 		verticalLayout.add(horizontalLayout);
 	}
-	
+
 	public void setFinalButtons() {
-		
-		viewLog.addThemeVariants(ButtonVariant.LUMO_PRIMARY);
-		viewLog.getStyle().set("background", "#58d2cc");
-		viewLog.getStyle().set("border-radius", "6px");
-		viewLog.getStyle().set("color", "#4b483f");
+
+//		viewLog.addThemeVariants(ButtonVariant.LUMO_PRIMARY);
+//		viewLog.getStyle().set("background", "#58d2cc");
+//		viewLog.getStyle().set("border-radius", "6px");
+//		viewLog.getStyle().set("color", "#4b483f");
+		viewLog.addClassName("add-button");
 		viewLog.getStyle().set("margin-left", "auto");
 
-		
-		cancel.addThemeVariants(ButtonVariant.LUMO_TERTIARY);
-		
-		back.addThemeVariants(ButtonVariant.LUMO_PRIMARY);
-		back.getStyle().set("background", "#58d2cc");
-		back.getStyle().set("border-radius", "6px");
-		back.getStyle().set("color", "#4b483f");
-		
+		cancel.addClassName("delete-button");
+
+//		cancel.addThemeVariants(ButtonVariant.LUMO_TERTIARY);
+
+//		back.addThemeVariants(ButtonVariant.LUMO_PRIMARY);
+//		back.getStyle().set("background", "#58d2cc");
+//		back.getStyle().set("border-radius", "6px");
+//		back.getStyle().set("color", "#4b483f");
+		back.addClassName("add-button");
+		finish.addClassName("delete-button");
+
 		back.addClickListener(e -> {
-			setContent(new UploadScreenTwo(this.pipelineConfigDao, this.channelConfigDao, this.commonConfigDao, pipelineName, filePath));
+			setContent(new UploadScreenTwo(this.pipelineConfigDao, this.channelConfigDao, this.commonConfigDao,
+					pipelineName, filePath));
 		});
-		
-		finish.addThemeVariants(ButtonVariant.LUMO_PRIMARY);
-		finish.getStyle().set("background", "#58d2cc");
-		finish.getStyle().set("border-radius", "6px");
-		finish.getStyle().set("color", "#4b483f");
+//		
+//		finish.addThemeVariants(ButtonVariant.LUMO_PRIMARY);
+//		finish.getStyle().set("background", "#58d2cc");
+//		finish.getStyle().set("border-radius", "6px");
+//		finish.getStyle().set("color", "#4b483f");
 		finish.getStyle().set("margin-right", "20px");
 
 		HorizontalLayout horizontalLayout = new HorizontalLayout();
@@ -229,7 +237,7 @@ public class UploadScreenThree extends AppLayout {
 		horizontalLayout.add(cancel);
 		horizontalLayout.add(back);
 		horizontalLayout.add(finish);
-		
+
 		verticalLayout.add(horizontalLayout);
 	}
 
